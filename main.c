9ae348177dc86b35413e446bfc4a4d8ed2dca91a
Paul Hey <heyp@camosun.bc.ca>
2014-04-11 16:07:15 -0700

    0.2: [W] PWM @100KHz (P1.0 LED1, P2.3 MOSFET).          MOSFET Switches 12V 1A DC Fan.      [T] Lower PWM Freq to ~25KHz. 0.1: [W] Echoes Back on USART @ 128Kbaud @ 16MHz.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
  * FanController - Version[0.1]
  *
  * [2014 - APR - 10]
+ * 0.2 =>
  * 0.1 =>	Echoes back on
  * 			USART @ 128000 BAUD.
  *
@@ -17,6 +18,8 @@ struct{
 	//TimerMode tm;
 	//unsigned char TxBitCount;
 	unsigned char TxBuffer;
+	unsigned int ms_Counter;//Keep track of current ms count
+	//unsigned Toggle:1;
 
 }GV;
 
@@ -33,13 +36,16 @@ void main(void) {
 
 	init();
 	SetupUSART_A0();
-	__bis_SR_register(LPM4_bits + GIE);
+	SetupTIMER_A0();
+	__bis_SR_register(LPM0_bits + GIE);
 
 	//return 0;
 }
 
 void init(void){
 	GV.TxBuffer = 0x00;
+	GV.ms_Counter = 0x0000;
+	//GV.Toggle = 0;
 
 	P1DIR = PxDIR_ALL_OUT;
 	P1OUT = PxOUT_ALL_OUT;
@@ -48,6 +54,8 @@ void init(void){
 
 	P2DIR = PxDIR_ALL_OUT;
 	P2OUT = PxOUT_ALL_OUT;
+	P3DIR = PxDIR_ALL_OUT;
+	P3OUT = PxOUT_ALL_OUT;
 
 }
 
@@ -61,10 +69,61 @@ void SetupUSART_A0(void){
 	IE2 |= UCA0RXIE;
 }
 
+void SetupTIMER_A0(void){
+	TA0CCR0 = TIMER0_A0_MAX;
+	TA0CCR1 = TIMER0_A0_MAX>>1;//For 50% Duty Cycle
+	TA0CCTL0 |= CCIE;
+	TA0CCTL1 |= CCIE;
+	TA0CTL = TASSEL_2 + MC_1 + TACLR;//SMCLK, UP, CLEAR
+}
+
 //ISRs
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0_RX_ISR(void){
 	while(!(IFG2&UCA0TXIFG));
 	UCA0TXBUF = UCA0RXBUF;
+}
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void){
+	//GV.ms_Counter++;
+	//if(GV.ms_Counter==MS_COUNTER_MAX){//Toggle every second
+
+		P1OUT |= LED1;
+		P2OUT |= MOSFET;
+	//	GV.ms_Counter = 0;//Reset Counter
+	//}
+
+}
+
+
+/* The TACCR1 CCIFG, TACCR2 CCIFG, and TAIFG flags are prioritized and combined to source a single
+ * interrupt vector. The interrupt vector register TAIV is used to determine which flag requested an interrupt.
+ * The highest priority enabled interrupt generates a number in the TAIV register (see register description).
+ * This number can be evaluated or added to the program counter to automatically enter the appropriate
+ * software routine. Disabled Timer_A interrupts do not affect the TAIV value.
+ * Any access, read or write, of the TAIV register automatically resets the highest pending interrupt flag. If
+ * another interrupt flag is set, another interrupt is immediately generated after servicing the initial interrupt.
+ * For example, if the TACCR1 and TACCR2 CCIFG flags are set when the interrupt service routine
+ * accesses the TAIV register, TACCR1 CCIFG is reset automatically. After the RETI instruction of the
+ * interrupt service routine is executed, the TACCR2 CCIFG flag will generate another interrupt.
+ * */
+
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void TIMER0_A1_ISR(void){
+	switch (__even_in_range(TA0IV, TA0IV_TAIFG)) {
+		case TA0IV_NONE: // No Interrupt pending
+			break;
+		case TA0IV_TACCR1: 	//TA0CCR1_CCIFG
+			P1OUT &= ~LED1;
+			P2OUT &= ~MOSFET;
+			break;
+		case TA0IV_TACCR2: 	//TA0CCR2_CCIFG
+		case TA0IV_6:		//Reserved
+		case TA0IV_8:		//Reserved
+		case TA0IV_TAIFG:	//TA0IFG
+		default:
+			break;
+	}
 }
--- main.h
+++ main.h
@@ -19,6 +19,11 @@
 #define LED1 BIT0
 #define LED2 BIT6
 
+#define MOSFET BIT3
+#define TIMER0_A0_MAX 160 //For ~0.01ms @ 16MHz
+#define TIMER0_A1_MAX 80 //For 50% DCY
+#define MS_COUNTER_MAX 1000 //For ~1s @ 16MHz
+
 #define LEDS_DIR P1DIR
 #define LEDS_OUT P1OUT
 
@@ -33,5 +38,6 @@ typedef enum{
 
 void init(void);
 void SetupUSART_A0(void);
+void SetupTIMER_A0(void);
 
 #endif /* MAIN_H_ */
